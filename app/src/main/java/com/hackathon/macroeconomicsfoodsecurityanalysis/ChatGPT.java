6f0dc89fea57fb4ad67d7357a012e20e4ed2f4d0
SaiKrishna221b <gudumusaikrishna@sjsu.edu>
2024-03-30 15:29:21 -0700

    custom chatGPT integration java and layout files

---
+++ app/src/main/java/com/hackathon/macroeconomicsfoodsecurityanalysis/ChatGPT.java
@@ -0,0 +1,161 @@
+package com.cmpe277.macroeconomicfoodsecurity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+
+public class ChatGPT extends Fragment {
+
+    RecyclerView recyclerView;
+    TextView welcomeTextView;
+    EditText messageEditText;
+    ImageButton sendButton;
+    List<Message> messageList;
+    MessageAdapter messageAdapter;
+    public static final MediaType JSON
+            = MediaType.get("application/json; charset=utf-8");
+    OkHttpClient client = new OkHttpClient();
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_chat_g_p_t, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        messageList = new ArrayList<>();
+
+        recyclerView = view.findViewById(R.id.recycler_view);
+        welcomeTextView = view.findViewById(R.id.welcome_text);
+        messageEditText = view.findViewById(R.id.message_edit_text);
+        sendButton = view.findViewById(R.id.send_btn);
+
+        //setup recycler view
+        messageAdapter = new MessageAdapter(messageList);
+        recyclerView.setAdapter(messageAdapter);
+        LinearLayoutManager llm = new LinearLayoutManager(this.getContext());
+        llm.setStackFromEnd(true);
+        recyclerView.setLayoutManager(llm);
+
+        sendButton.setOnClickListener((v) -> {
+            String question = messageEditText.getText().toString().trim();
+            addToChat(question, Message.SENT_BY_ME);
+            messageEditText.setText("");
+            callAPI(question);
+            welcomeTextView.setVisibility(View.GONE);
+        });
+    }
+
+    //    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.fragment_chat_g_p_t);
+//
+//    }
+
+    void addToChat(String message, String sentBy) {
+
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                messageList.add(new Message(message, sentBy));
+                messageAdapter.notifyDataSetChanged();
+                recyclerView.smoothScrollToPosition(messageAdapter.getItemCount());
+            }
+        });
+    }
+
+    void addResponse(String response) {
+        messageList.remove(messageList.size() - 1);
+        addToChat(response, Message.SENT_BY_BOT);
+    }
+
+    void callAPI(String question) {
+        //okhttp
+        messageList.add(new Message("Typing... ", Message.SENT_BY_BOT));
+
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("model", "gpt-3.5-turbo");
+            jsonBody.put("max_tokens", 4000);
+            jsonBody.put("temperature", 0);
+            JSONArray promptArray = new JSONArray();
+            JSONObject promptObject = new JSONObject();
+            promptObject.put("role", "user");
+            promptObject.put("content", question);
+            promptArray.put(promptObject);
+            jsonBody.put("messages", promptArray);
+        } catch (JSONException e) {
+            throw new Error(e);
+//            e.printStackTrace();
+        }
+        RequestBody body = RequestBody.create(jsonBody.toString(), JSON);
+        Request request = new Request.Builder()
+                .url("https://api.openai.com/v1/chat/completions")
+                .header("Authorization", "Bearer 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")
+                .header("Assistant-ID", "asst_SjcCrbv8t58pd5fmdSH9D2pP")
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                addResponse("Failed to load response due to " + e);
+                throw new Error(e);
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    JSONObject jsonObject = null;
+                    try {
+                        jsonObject = new JSONObject(response.body().string());
+                        JSONArray jsonArray = jsonObject.getJSONArray("choices");
+                        JSONObject result = jsonArray.getJSONObject(0).getJSONObject("message");
+                        String result1 = result.getString("content");
+                        addResponse(result1.trim());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+
+                } else {
+                    addResponse("Failed to load response due to " + response.body().toString());
+                }
+            }
+        });
+
+
+    }
+
+}
